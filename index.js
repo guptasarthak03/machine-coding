68a286205e1bc1d7917c2935ed307a71d4523e6c
guptasarthak03 <guptasarthak03@gmail.com>
2023-11-23 17:39:36 +0530

    run parcel, gitignore, react use

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Hello world from Javscript --------------------------------------------
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// ! Hello world from Javscript --------------------------------------------
 const heading = document.createElement('h1');
 heading.textContent = 'Hello World from Javascript';
 
@@ -6,9 +9,9 @@ const rootDOM = document.getElementById('root');
 rootDOM.appendChild(heading);
 
 
-// Hello world from React (it will replace anything inside the root "the element we run react in") --------------------------------------------------
+// ! Hello world from React (it will replace anything inside the root "the element we run react in") --------------------------------------------------
 const headingReact = React.createElement('h1', {id: 'heading'}, 'Hello From React');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(headingReact);
 
-console.log(headingReact, '<--'); // This is a plain JS Object
+// console.log(headingReact, '<--'); // * This is a plain JS Object
