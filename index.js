e6e675981c716a00f1f7ddee26024886aa1120ee
guptasarthak03 <guptasarthak03@gmail.com>
2023-11-23 18:23:55 +0530

    concept: react element, html element, fn comp

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './pages/App';
 
-// ! Hello world from Javscript --------------------------------------------
-const heading = document.createElement('h1');
-heading.textContent = 'Hello World from Javascript';
+// // ! Hello world from Javscript --------------------------------------------
+// const heading = document.createElement('h1');
+// heading.textContent = 'Hello World from Javascript';
 
-const rootDOM = document.getElementById('root');
-rootDOM.appendChild(heading);
+// const rootDOM = document.getElementById('root');
+// rootDOM.appendChild(heading);
 
+// // ! Hello world from React (it will replace anything inside the root "the element we run react in") --------------------------------------------------
+// // ? React.creatElement  => React Element (JS Object) => HTMLElement(render)
+// // heading from react core
+// const headingReact = React.createElement(
+//   'h1',
+//   { id: 'heading' },
+//   'Hello From React'
+// );
 
-// ! Hello world from React (it will replace anything inside the root "the element we run react in") --------------------------------------------------
-const headingReact = React.createElement('h1', {id: 'heading'}, 'Hello From React');
+// // heading from react JSX
+// const headingJSX = <h1 className='heading'>Hello From JSX</h1>;
+
+// console.log(headingReact, headingJSX, 'object'); // * React Element
+
+// // * React functional Component
+// const HeadingComponent = () => {
+//   return <h1 className='heading'>React functional component</h1>;
+// };
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(headingReact);
 
-// console.log(headingReact, '<--'); // * This is a plain JS Object
+// root.render(headingJSX);
+// root.render(<HeadingComponent />)
+
+root.render(<App />)
